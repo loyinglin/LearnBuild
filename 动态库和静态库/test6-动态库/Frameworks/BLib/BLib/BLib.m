8bed62c705113734dabbb2d3aff20d5e63c1b2ec
林伟池 <loying@foxmail.com>
2016-12-22 11:05:03 +0800

    静态库

---
+++ 动态库和静态库/test6-动态库/Frameworks/BLib/BLib/BLib.m
@@ -0,0 +1,15 @@
+//
+//  BLib.c
+//  testLib
+//
+//  Created by linjiansheng on 12/19/16.
+//  Copyright © 2016 youshixiu. All rights reserved.
+//
+
+#include <stdio.h>
+#include "ALib.h"
+void call_foo_b()
+{
+    printf("call_foo in BLib.\n");
+    foo();
+}
